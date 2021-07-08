968ca7fbe905c388d4320c727de507ffceb1d392
changangus <63685366+changangus@users.noreply.github.com>
2021-07-08 15:24:57 -0400

    added chakra ui

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+
+interface NavbarProps {
+
+}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+    return (
+      <Button colorScheme="blue">Button</Button>
+    );
+}
+
+export default Navbar;
