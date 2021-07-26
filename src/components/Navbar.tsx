2da9fcf9bc63dadf4eae73785738c09586d2e98f
changangus <63685366+changangus@users.noreply.github.com>
2021-07-25 20:39:41 -0400

    created links to register and login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,31 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import React from 'react';
 interface NavbarProps {
 
 }
 
-const Navbar: React.FC<NavbarProps> = ({}) => {
-    return (
-      <Flex bg='#dcdcde' padding={4} justifyContent='space-between'>
-        <Box>
-          Hello Logo
-        </Box>
-        <Box bg='white' border='1px black solid' h='24px' w='350px' ></Box>
-        <Flex w='10%' justifyContent='space-evenly'>
-          <Box>Login</Box>
-          <Box>Register</Box>
-        </Flex>
+const Navbar: React.FC<NavbarProps> = ({ }) => {
+  return (
+    <Flex bg='#dcdcde' padding={4} justifyContent='space-between'>
+      <Box>
+        Hello Logo
+      </Box>
+      <Box bg='white' border='1px black solid' h='24px' w='350px' ></Box>
+      <Flex w='10%' justifyContent='space-evenly'>
+        <NextLink href='/login'>
+          <Link>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href='/register'>
+          <Link>
+            Register
+          </Link>
+        </NextLink>
       </Flex>
-    );
+    </Flex>
+  );
 }
 
 export default Navbar;
---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface loginProps {
+
+}
+
+const login: React.FC<loginProps> = ({}) => {
+    return (
+        <div></div>
+    );
+}
+
+export default login;
